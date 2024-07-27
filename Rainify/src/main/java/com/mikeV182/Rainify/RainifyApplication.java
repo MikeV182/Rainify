90d8644ccfbbdefcb2d9dadf324e1612d779c846
Mike Vays <michailvays@gmail.com>
2024-07-27 17:20:59 +0700

    Initialized the project

---
+++ Rainify/src/main/java/com/mikeV182/Rainify/RainifyApplication.java
@@ -0,0 +1,13 @@
+package com.mikeV182.Rainify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainifyApplication.class, args);
+	}
+
+}
---
+++ Rainify/src/test/java/com/mikeV182/Rainify/RainifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikeV182.Rainify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
